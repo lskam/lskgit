47cd66670cb16344e0d8857ba1d8fc97f055320c
root <lskam@163.com>
2018-10-06 17:55:25 -0700

    modify shm

--- linux_c_test/173655bf00008ac351/source code/chapter_7/test7_3a.c
+++ linux_c_test/173655bf00008ac351/source code/chapter_7/test7_3a.c
@@ -4,14 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUF_SIZE 1024
-#define MYKEY 24
+#define BUF_SIZE 4096
+#define MYKEY 2400
 
 int main(void)
 {
     int shmid;
     char *shmptr;
-    if(shmid=shmget(MYKEY,BUF_SIZE,IPC_CREAT)==-1)
+	shmid=shmget(MYKEY,BUF_SIZE,IPC_CREAT);
+	printf("test:%d\r\n",shmid);
+    if(shmid==-1)
     {
         printf("shmget error!\n");
         exit(1);
@@ -25,7 +27,7 @@ int main(void)
     {
         printf("string:%s\n",shmptr);
 
-        sleep(30);
+        sleep(3);
     }
     exit(0);
 }
--- linux_c_test/173655bf00008ac351/source code/chapter_7/test7_3b.c
+++ linux_c_test/173655bf00008ac351/source code/chapter_7/test7_3b.c
@@ -4,15 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUF_SIZE 1024
-#define MYKEY 24
+#define BUF_SIZE 4096
+#define MYKEY 2400
 
 int main(void)
 {
     int shmid;
     char *shmptr;
-
-    if(shmid=shmget(MYKEY,BUF_SIZE,IPC_CREAT)==-1)
+	shmid=shmget(MYKEY,BUF_SIZE,IPC_CREAT);
+	printf("shmid:%d\r\n",shmid);
+    if(shmid==-1)
     {
         printf("shmget error!\n");
         exit(1);
